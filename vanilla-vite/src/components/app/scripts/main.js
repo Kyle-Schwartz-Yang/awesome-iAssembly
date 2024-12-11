4f11744cbf0557663e1d807403f03125375d95b0
schwartz <gtfsolo@gmail.com>
2024-12-11 23:04:58 +0200

    Settings project vanilla-vite

---
+++ vanilla-vite/src/components/app/scripts/main.js
@@ -0,0 +1,17 @@
+// ----------------------------NO-Modules (START)
+
+import { initHeaderMenu } from "../../widgets/Header/_Header.js";
+import { initAccordion } from "../../shared/components/Accordion/_Accordion.js";
+import { initEmployes } from "./modules/initEmployes.js";
+// import { initSerchEmployees } from "../../widgets/SearchPanel/_searchPanel.js";
+
+// ----------------------------NO-Modules (END)
+
+export const modules = {
+  // -------------------NO-Modules
+  initHeaderMenu: initHeaderMenu,
+  initAccordion: initAccordion,
+  initEmployes: initEmployes,
+
+  // -------------------NO-Modules
+};
