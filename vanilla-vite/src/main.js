c094efc613ebdc9af65e5a987e2b4e86ac808b32
schwartz <gtfsolo@gmail.com>
2024-12-12 23:13:58 +0200

    скcreate first component tabbed

--- vanilla-vite/src/main.js
+++ vanilla-vite/src/main.js
@@ -1,1 +1,15 @@
 import "./components/app/app.scss";
+import { Tabbed } from "./components/features/tab/tab";
+
+function initTab() {
+  const cfg = {
+    tab: document.querySelector(".tab"),
+    buttons: document.querySelectorAll(".tab__button"),
+    contents: document.querySelectorAll(".tab__txt"),
+  };
+
+  const tabbed = new Tabbed(cfg);
+  tabbed.init();
+}
+
+initTab();
