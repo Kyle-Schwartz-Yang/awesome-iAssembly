8afd2ae0150f353195b39a9ddc65c233b04efc04
schwartz <gtfsolo@gmail.com>
2024-12-13 15:28:05 +0200

    settings import/export and JSDOC

--- vanilla-vite/src/components/app/app.js
+++ vanilla-vite/src/components/app/app.js
@@ -1,1 +1,9 @@
-import "../features/tab/tab";
+import { initTab } from "../features/tab/initTab";
+
+export const initApp = () => {
+  initTab({
+    tab: document.querySelector(".tab"),
+    buttons: document.querySelectorAll(".tab__button"),
+    contents: document.querySelectorAll(".tab__txt"),
+  });
+};
