aac544a39f7181df3c7a3bb62162423ab0a4acc7
schwartz <gtfsolo@gmail.com>
2024-12-14 03:57:34 +0200

    optimizated images set plugin

--- vanilla-vite/src/main.js
+++ vanilla-vite/src/main.js
@@ -5,3 +5,9 @@ import { initApp } from "./components/app/app";
 document.addEventListener("DOMContentLoaded", () => {
   initApp();
 });
+
+import viteLogo from "@/body.png";
+
+const image = (document.querySelector("#image").src = viteLogo);
+
+console.log(image);
