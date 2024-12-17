e174e6c64f2dbd4b244e1057c27d083be77a6e0e
schwartz <gtfsolo@gmail.com>
2024-12-17 23:18:52 +0200

    vanilla-vite-multipage

---
+++ vanilla-vite-multipage/src/main.js
@@ -0,0 +1,7 @@
+import "./components/app/app.scss";
+// ------------------------------------------
+
+import logo from "@/vite.svg";
+
+const logos = document.querySelector("#images");
+logos.src = logo;
